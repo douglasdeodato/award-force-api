4de6c3d4092df27b1041ee37e53535836a3e379d
douglas <douglasdeodato@gmail.com>
2023-09-20 11:41:19 +0100

    first test

---
+++ get-user-by-email/get-user-by-email.py
@@ -0,0 +1,35 @@
+import requests
+import json
+import os
+from dotenv import load_dotenv
+
+# Define the path to the root directory where .env is located
+root_dir = os.path.join(os.path.dirname(__file__), '..', '..')  # Adjust the number of '..' as needed
+
+# Load environment variables from the .env file in the root directory
+dotenv_path = os.path.join(root_dir, '.env')
+load_dotenv(dotenv_path)
+
+# Get the API key and URL from the environment variables
+api_key = os.getenv("API_KEY")
+api_url = os.getenv("API_URL")
+
+# Define the headers
+headers = {
+    'Accept': 'application/vnd.Creative Force.v2.1+json',
+    'x-api-key': api_key
+}
+
+# Send the GET request
+response = requests.get(api_url, headers=headers)
+
+# Check if the request was successful (status code 200)
+if response.status_code == 200:
+    # Parse the JSON response
+    data = response.json()
+    
+    # Print or save the JSON data
+    print(json.dumps(data, indent=4))  # Pretty print the JSON
+else:
+    print(f"Request failed with status code: {response.status_code}")
+    print(response.text)  # Print the response content if there's an error
