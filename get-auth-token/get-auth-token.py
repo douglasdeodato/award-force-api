5cd93c6ba9204f9f4058a760e71637fb83da8098
douglas <douglasdeodato@gmail.com>
2023-09-20 12:46:17 +0100

    print the json

--- get-auth-token/get-auth-token.py
+++ get-auth-token/get-auth-token.py
@@ -1,4 +1,5 @@
 import requests
+import json
 import os
 from dotenv import load_dotenv
 
@@ -22,9 +23,16 @@ response = requests.get(api_url_external, headers=headers)
 if response.status_code == 200:
     # Parse the JSON response
     data = response.json()
-    
-    # Print the JSON response
-    print(data)
+
+    # Define the folder path and filename for the JSON file
+    folder_path = os.path.dirname(__file__)  # Get the path of the current script
+    json_filename = os.path.join(folder_path, 'output.json')  # Adjust the filename as needed
+
+    # Write the JSON data to the file
+    with open(json_filename, 'w') as json_file:
+        json.dump(data, json_file, indent=4)
+
+    print(f"JSON data has been saved to {json_filename}")
 else:
     print(f"Request failed with status code: {response.status_code}")
     print(response.text)  # Print the response content if there's an error
